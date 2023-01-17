12202f86761caa0fe71f9ba795e620ca3d344961
Areeeb98 <122514572+Areeeb98@users.noreply.github.com>
2023-01-18 00:27:32 +0530

    Add files via upload

---
+++ Type_conversions_and_casting.py
@@ -0,0 +1,87 @@
+"""
+Day: Tuesday
+Date: 17th Jan 2023
+Time: 11 PM
+Topic: Types of conversions and casting in python
+Author: Areeb Ahmed
+"""
+
+
+# a=7
+# print(a,type(a))
+# b=float(a)
+# print(b,type(b))
+# c=str(a)
+# print(c,type(c))
+
+x=7
+# print("The value of x is",x)
+# print("The value of x is %d"%x)
+# print("The value of x is"+' '+str(x))
+
+''' convert float to int and str '''
+q=7.5
+# print(q,type(q))
+# w=int(q)
+# print(w,type(w))
+# e=str(q)
+# print(e,type(e))
+
+''' converting str to int and float'''
+t="ameerpet" # if the value inside the string is integer it can be converted to int
+# print(t,type(t))
+# y=int(t) # value error
+# z=float(t) # value error
+
+# p='123.9'
+# print(p,type(p))
+''' a=int(p) # this wont work''' 
+# s=float(p)
+# print(s,type(s))
+# v=(int(s))
+# print(v,type(v))
+
+# a=4+5
+# print(a,type(a))
+# b=0.1+0.2
+# print(b)
+# print("%f"%b,type(b))
+
+# c="Python Programmig"
+# print(c,type(c))
+
+# y='Josh' ; j='hey'
+# print(y,type(y))
+# print("%s" %(j))
+
+# k=57
+# print("%c"%k)
+# e='Y'
+# print("%c"%e)
+
+# print(chr(0)+"python")
+# print(chr(32)+"Areeb Ahmed")
+
+# a=4
+# b=float(a)
+# print(b)
+
+# c=8.4
+# d=(int(c))
+# print(d)
+
+e='josh'
+'''f=int(e) # ValueError'''
+'''g=float(e) # ValueError'''
+
+# h='56'
+# i=int(h)
+# print(h)
+# j=float(h)
+# print(j)
+# k=int(j)
+# print(k)
+
+p=6 ; r=4.5
+s=p+r
+print(s,type(s))
